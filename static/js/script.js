81ee4a428b83a76dea8c331eb7c916923da0b7a2
root <admin@realitygroup.ru>
2011-08-19 16:31:28 +0400

    static index page

---
+++ static/js/script.js
@@ -0,0 +1,27 @@
+/* Author: RealityGroup http://realitygroup.ru
+
+*/
+
+$(function(){
+	Cufon.replace('.font-replace', { fontFamily: 'Myriad Pro Regular', textShadow: '1px 1px #eee', hover: true });
+	//Cufon.now();
+	
+	$('div#slides').cycle({
+		fx: 'fade',
+		speed: '2000',
+		easing: 'easeInOutExpo',
+		timeout: 9000,
+		prev: '#slide-prev',
+		next: '#slide-next',
+		width: 552,
+		fit: 1
+	}); 	
+	
+	$('.scroll-pane').jScrollPane({
+		showArrows: true,
+		verticalDragMinHeight: 30,
+		verticalDragMaxHeight: 80
+	});	
+});
+
+
